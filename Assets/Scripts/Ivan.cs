46dc560263221236987ae6c166fa39ba6c5c01b7
AshDeal-Marco <ashdell31@gmail.com>
2026-05-03 17:21:46 +0330

    Started to get the input system under control

--- Assets/Scripts/Ivan.cs
+++ Assets/Scripts/Ivan.cs
@@ -3,16 +3,26 @@ using UnityEngine.InputSystem;
 
 public class Ivan : MonoBehaviour
 {
+    public float moveSpeed = 0f;
     
-    float moveSpeed = 3f;
-    bool isAlive = true;
-
-    Vector2 move;
-    bool jump = false;
     [Space(5)]
     public Rigidbody2D rb;
     public InputActionReference moveAction;
     public InputActionReference interactAction;
-    public InputActionReference sprintAction;
 
+    //----------public-variables-upside-----private-variables-downside-------------
+    bool isAlive = true;
+    bool isInteracting = false;
+    Vector2 moveDirection;
+
+    private void Update()
+    {
+        moveDirection = moveAction.action.ReadValue<Vector2>();
+        Debug.LogWarning(moveDirection);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
 }
