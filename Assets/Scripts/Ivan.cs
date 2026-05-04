35ca5b733aa91407f4ad24eeb6ae6a56bad4ad5b
AshDeal-Marco <ashdell31@gmail.com>
2026-05-04 14:25:34 +0330

    Been messing with physics
    Added a a few functions to player script so that it can flip the player sprite and to be able to recognize the nearest moveable object. I also added a few tags and layers to control game objects in the scene
    don't know if any of it will prove usefull in the future.

--- Assets/Scripts/Ivan.cs
+++ Assets/Scripts/Ivan.cs
@@ -3,33 +3,76 @@ using UnityEngine.InputSystem;
 
 public class Ivan : MonoBehaviour
 {
+    #region Variables
     public float moveSpeed = 0f;
-    
+    public float interactionRange = 0f;
     [Space(5)]
     public Rigidbody2D rb;
     public InputActionReference moveAction;
     public InputActionReference interactAction;
 
-    //----------public-variables-upside-----private-variables-downside-------------
+    //----------public-variables-upside-----private-variables-downside-----------
+
     bool isAlive = true;
-    bool isInteracting = false;
     Vector2 moveDirection;
+
+    GameObject nearbyMoveableObject = null;
+    #endregion
 
     private void Update()
     {
-        moveDirection = moveAction.action.ReadValue<Vector2>();
-        Debug.LogWarning(moveDirection);
+        if (!isAlive) return;
+
+        moveDirection = moveAction.action.ReadValue<Vector2>(); // Read move input
+
+        if (interactAction.action.triggered) // triggered is true when the action state changes to being pressed
+            FindNearestObject(); // Finds the nearest object that is moveable
     }
     
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, rb.linearVelocity.y);
+        FlipSprite();
+    }
 
-        #region Flip sprite
+    //-------------Functions---------------------------------------------------
+    
+    private void FlipSprite()
+    {
         if (moveDirection.x > 0)
             transform.localScale = new Vector3(1, 1, 1);
         else if (moveDirection.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
-        #endregion
+    }
+
+    private void FindNearestObject()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, interactionRange);
+
+        GameObject closestObject = null;
+        float minDistance = interactionRange;
+
+        foreach (Collider2D hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("Moveable"))
+            {
+                Vector3 closestPoint = hitCollider.ClosestPoint(transform.position);
+                float distance = Vector2.Distance(transform.position, closestPoint);
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestObject = hitCollider.gameObject;
+                }
+            }
+        }
+
+        nearbyMoveableObject = closestObject;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, interactionRange);
     }
 }
